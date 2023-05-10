eebac6c2934eea0b9622b7c21ba45e84a27cfc84
oaaees <jmpareds@gmail.com>
2023-05-10 01:35:09 -0400

    added .gitignore file

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,11 @@
 #include <FL/Fl_Box.H>
 
 int main(int argc, char **argv) {
-    Fl_Window *window = new Fl_Window(340, 180, "Te amo alejandra");
-    Fl_Box *box = new Fl_Box(20, 40, 300, 100, "ola alejandra t pareces a trucha");
+    Fl_Window *window = new Fl_Window(680, 360, "undefined");
+    Fl_Box *box = new Fl_Box(40, 80, 600, 200, "ola alejandra t pareces a trucha");
     box->box(FL_UP_BOX);
     box->labelfont(FL_BOLD+FL_ITALIC);
-    box->labelsize(16);
+    box->labelsize(32);
     box->labeltype(FL_NORMAL_LABEL);
     window->end();
     window->show(argc, argv);
